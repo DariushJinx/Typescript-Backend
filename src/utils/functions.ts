21b07b9b9771443d841c472bdb8d0cb9bc3288a6
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-09-16 16:22:21 +0330

    handle functions for project

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,7 +1,39 @@
-function RandomNumberGenerator() {
-  return ~~(Math.random() * 90000 + 10000);
-}
+import JWT from "jsonwebtoken";
+import { constants } from "./constans.utils";
+import { JwtToken } from "../http/types/public/public.types";
+import { compareSync } from "bcrypt";
+import path from "path";
+import fs from "fs";
 
-export const UtilsFunctions = {
-  RandomNumberGenerator,
-};
+export class FunctionUtils {
+  public static RandomNumberGenerator(): number {
+    return ~~(Math.random() * 90000 + 10000);
+  }
+  public static SignAccessToken(payload: JwtToken): string {
+    const options = {
+      expiresIn: "30d",
+    };
+    return JWT.sign(payload, constants.ACCESS_TOKEN_SECRET_KEY, options);
+  }
+
+  public static comparePassword(password: string, hashedPassword: string): boolean {
+    return compareSync(password, hashedPassword);
+  }
+
+  public static ListOfImagesForRequest(files: Express.Multer.File[], fileUploadPath: string) {
+    if (files) {
+      return files
+        .map((file) => path.join(`http://127.0.0.1:8888/`, fileUploadPath, file.filename))
+        .map((item) => item.replace(/\\/g, "//"));
+    } else {
+      return [];
+    }
+  }
+
+  public static deleteFileInPublic(fileAddress: string): void {
+    if (fileAddress) {
+      const pathFile = path.join(__dirname, "..", "..", "public", fileAddress);
+      if (fs.existsSync(pathFile)) fs.unlinkSync(pathFile);
+    }
+  }
+}
