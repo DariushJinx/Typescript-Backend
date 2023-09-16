d38d5c8e578e41395b8e5f3e763e618f7121537a
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-09-16 16:09:10 +0330

    file updated with register and login dto

--- src/http/dtos/auth/auth.dto.ts
+++ src/http/dtos/auth/auth.dto.ts
@@ -1,11 +1,61 @@
 import { Expose } from "class-transformer";
-import { IsDefined, Matches, Length, IsNotEmpty } from "class-validator";
+import { IsDefined, Matches, Length, IsNotEmpty, IsEmail } from "class-validator";
 
 export class GetOtpDto {
   @IsDefined()
   @Expose()
   @Matches(RegExp(/^09\d{9}$/), { message: "فرمت شماره تماس وارد شده صحیح نمی باشد" })
   @Length(11, 11, { message: "تعداد کاراکترهای شماره تماس باید 11 عدد باشد" })
-  @IsNotEmpty({ message: "شماره تماس وارد شده نباید خالی باشد" })
   mobile: string;
+}
+export class CheckOtpDto {
+  @IsDefined()
+  @Expose()
+  @Matches(RegExp(/^09\d{9}$/), { message: "فرمت شماره تماس وارد شده صحیح نمی باشد" })
+  @Length(11, 11, { message: "تعداد کاراکترهای شماره تماس باید 11 عدد باشد" })
+  mobile: string;
+  @IsDefined()
+  @Expose()
+  @Length(5, 5, { message: "کد احراز هویت وارد شده صحیح نمی باشد" })
+  code: number;
+}
+
+export class RegisterDto {
+  @IsDefined()
+  @Expose()
+  @Length(3, 11, { message: "تعداد کاراکترهای  یوزرنیم باید بین 3 تا 11  باشد" })
+  username: string;
+  @IsDefined()
+  @Expose()
+  first_name: string;
+  @IsDefined()
+  @Expose()
+  last_name: string;
+  @IsDefined()
+  @Expose()
+  @IsEmail()
+  @Matches(
+    /^[a-z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)*$/i,
+    { message: "ایمیل وارد شده صحیح نمی باشد" }
+  )
+  email: string;
+  @IsDefined()
+  @Expose()
+  password: string;
+  @IsDefined()
+  @Expose()
+  @Matches(RegExp(/^09\d{9}$/), { message: "فرمت شماره تماس وارد شده صحیح نمی باشد" })
+  @Length(11, 11, { message: "تعداد کاراکترهای شماره تماس باید 11 عدد باشد" })
+  @IsNotEmpty({ message: "تعداد کاراکترهای شماره تماس باید 11 عدد باشد" })
+  mobile: string;
+}
+
+export class LoginDto {
+  @IsDefined()
+  @Expose()
+  @Length(3, 11, { message: "تعداد کاراکترهای  یوزرنیم باید بین 3 تا 11  باشد" })
+  username: string;
+  @IsDefined()
+  @Expose()
+  password: string;
 }
