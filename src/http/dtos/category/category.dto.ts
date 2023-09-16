6f50bd31266298e535e823edd28bac13d7d7d677
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-09-16 16:10:16 +0330

    handle dto for category

---
+++ src/http/dtos/category/category.dto.ts
@@ -0,0 +1,33 @@
+import { Expose } from "class-transformer";
+import { IsDefined, Matches, Length, Allow, IsOptional } from "class-validator";
+import { constants } from "../../../utils/constans.utils";
+export class CreateCategoryDto {
+  @IsDefined()
+  @Expose()
+  @Length(3, 30, { message: "عنوان دسته بندی وارد شده صحیح نمی باشد" })
+  title: string;
+  @Expose()
+  @Allow()
+  @IsOptional()
+  @Matches(constants.MongoIDPattern, { message: "شناسه وارد شده صحیح نمی باشد" })
+  parent?: string;
+  @Expose()
+  @IsOptional()
+  @Matches(/(\.png|\.jpg|\.webp|\.jpeg|\.gif)$/, { message: "فرمت عکس ارسالی صحیح نمی باشد" })
+  filename?: string;
+  @Allow()
+  fileUploadPath: string;
+}
+
+export class UpdateBlogDto {
+  @IsDefined()
+  @Expose()
+  @Length(3, 30, { message: "عنوان دسته بندی وارد شده صحیح نمی باشد" })
+  title: string;
+  @IsDefined()
+  @Expose()
+  @Matches(/(\.png|\.jpg|\.webp|\.jpeg|\.gif)$/, { message: "فرمت عکس ارسالی صحیح نمی باشد" })
+  filename: string;
+  @Allow()
+  fileUploadPath: string;
+}
