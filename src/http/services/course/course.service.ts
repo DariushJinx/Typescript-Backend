9f841cfd4d6d2795d3769fe78f76badb7b3af594
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-09-16 23:47:38 +0330

    handle service for course

---
+++ src/http/services/course/course.service.ts
@@ -0,0 +1,50 @@
+import createHttpError from "http-errors";
+import { CourseModel } from "../../models/course/course.model";
+import { ICourse } from "../../types/course/course.types";
+
+export class CourseService {
+  async getOne(title: string): Promise<ICourse> {
+    const course: ICourse | null = await CourseModel.findOne({ title: title })
+      .populate([
+        { path: "category", select: { title: 1 } },
+        { path: "teacher", select: { first_name: 1, last_name: 1, mobile: 1, email: 1 } },
+        { path: "students", select: { first_name: 1, last_name: 1, mobile: 1, email: 1 } },
+        {
+          path: "likes",
+          select: {
+            first_name: 1,
+            last_name: 1,
+            username: 1,
+            role: 1,
+            _id: 0,
+          },
+        },
+        {
+          path: "dislikes",
+          select: {
+            first_name: 1,
+            last_name: 1,
+            username: 1,
+            role: 1,
+            _id: 0,
+          },
+        },
+        {
+          path: "bookmarks",
+          select: {
+            first_name: 1,
+            last_name: 1,
+            username: 1,
+            role: 1,
+            _id: 0,
+          },
+        },
+      ])
+      .lean();
+    if (course) {
+      return course;
+    } else {
+      throw createHttpError.NotFound("دوره مورد نظر یافت نشد");
+    }
+  }
+}
