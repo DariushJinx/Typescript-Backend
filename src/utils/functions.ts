574f59d31e18b640b01c1d25313b46200d5cecad
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-09-16 23:49:28 +0330

    file updated with functions for routes

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -4,6 +4,11 @@ import { JwtToken } from "../http/types/public/public.types";
 import { compareSync } from "bcrypt";
 import path from "path";
 import fs from "fs";
+import mongoose from "mongoose";
+import createHttpError from "http-errors";
+import { CourseModel } from "../http/models/course/course.model";
+import { Response } from "express";
+import { StatusCodes } from "http-status-codes";
 
 export class FunctionUtils {
   public static RandomNumberGenerator(): number {
@@ -36,4 +41,45 @@ export class FunctionUtils {
       if (fs.existsSync(pathFile)) fs.unlinkSync(pathFile);
     }
   }
+
+  public static async findCourseById(id: string) {
+    if (!mongoose.isValidObjectId(id))
+      throw createHttpError.BadRequest("شناسه ارسال شده صحیح نمیباشد");
+    const course = await CourseModel.findById(id);
+    if (!course) throw createHttpError.NotFound("دوره ای یافت نشد");
+    return course;
+  }
+
+  public static copyObject(object: object) {
+    return JSON.parse(JSON.stringify(object));
+  }
+
+  public static deleteInvalidPropertyInObject(
+    data: { [key: string]: any } = {},
+    blackList: string[] = []
+  ) {
+    const nullishData = ["", " ", "0", 0, null, undefined];
+    Object.keys(data).forEach((key) => {
+      if (blackList.includes(key)) delete data[key];
+      if (typeof data[key] == "string") data[key] = data[key].trim();
+      if (Array.isArray(data[key]) && data[key].length > 0)
+        data[key] = data[key].map((item: any) => item.trim());
+      if (Array.isArray(data[key]) && data[key].length == 0) delete data[key];
+      if (nullishData.includes(data[key])) delete data[key];
+    });
+  }
+
+  public static async findCourseWithTitleOrID (field:string,res:Response) {
+    const findQuery = mongoose.isValidObjectId(field) ? { _id: field } : { title: field };
+    const course = await CourseModel.findOne(findQuery);
+    if (!course) {
+      return res.status(StatusCodes.NOT_FOUND).json({
+        statusCode: StatusCodes.NOT_FOUND,
+        data: {
+          message: "دوره مورد نظر یافت نشد",
+        },
+      });
+    }
+    return course;
+  };
 }
