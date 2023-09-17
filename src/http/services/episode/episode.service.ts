aaef12c892a8424a895caa08a51ab68adcfcb8a8
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-09-17 21:29:19 +0330

    file updated with handle types

--- src/http/services/episode/episode.service.ts
+++ src/http/services/episode/episode.service.ts
@@ -1,6 +1,8 @@
+import { IEpisode } from "../../types/episode/episode.types";
+
 export class EpisodeService {
-  async getEpisode(model: any, id: string) {
-    const findEpisode = await model.findOne(
+  async getEpisode(model: any, id: string):Promise<IEpisode> {
+    const findEpisode:any = await model.findOne(
       { "chapters.episodes._id": id },
       { "chapters.episodes.$": 1 }
     );
