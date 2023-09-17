6d8bf44b5460ebf7ff90c8731d82acab79770306
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-09-17 21:29:25 +0330

    file updated with handle types

--- src/http/services/product/product.service.ts
+++ src/http/services/product/product.service.ts
@@ -6,23 +6,15 @@ import mongoose from "mongoose";
 import { FunctionUtils } from "../../../utils/functions";
 
 export class ProductService {
-  async findProductWitId(id: string): Promise<IProduct> {
+  async findProductWitId(id: string): Promise<IProduct | null> {
     const product: IProduct | null = await ProductModel.findById(id);
-    if (product) {
-      return product;
-    } else {
-      throw createHttpError.NotFound("محصول مورد نظر یافت نشد");
-    }
+    return product;
   }
 
-  async findProductWithIDOrTitle(field: string): Promise<IProduct> {
+  async findProductWithIDOrTitle(field: string): Promise<IProduct | null> {
     const findQuery = mongoose.isValidObjectId(field) ? { _id: field } : { title: field };
     const product: IProduct | null = await ProductModel.findOne(findQuery).lean();
-    if (product) {
-      return product;
-    } else {
-      throw createHttpError.NotFound("محصول مورد نظر یافت نشد");
-    }
+    return product;
   }
 
   async findFeatureInFeatures(productID: string, title: string): Promise<IProduct> {
