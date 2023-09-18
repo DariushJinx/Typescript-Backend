04922668956bae743ba69c41608d8953d39b2812
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-09-19 01:42:51 +0330

    handle swagger for info

---
+++ src/http/swaggers/info/info.swagger.ts
@@ -0,0 +1,21 @@
+/**
+ * @swagger
+ *  /info/p-admin :
+ *      get :
+ *          tags : [Info(PanelAdmin)]
+ *          summary : get infos for admin
+ *          responses :
+ *              200 :
+ *                  description : success
+ */
+
+/**
+ * @swagger
+ *  /info/admins :
+ *      get :
+ *          tags : [Info(PanelAdmin)]
+ *          summary : get all admins
+ *          responses :
+ *              200 :
+ *                  description : success
+ */
